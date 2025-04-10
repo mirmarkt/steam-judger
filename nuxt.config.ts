df942b76c41b8bc288e0ac8a0cc4be9bbb8ef0ff
Kutius <precious.wei@qq.com>
2025-04-11 04:04:21 +0800

    fix(nuxt.config): 移除prerender配置中的ignore选项并添加autoSubfolderIndex
    移除不再需要的`ignore: ['/hi']`配置，并添加`autoSubfolderIndex: false`以优化预渲染行为。

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -58,7 +58,7 @@ export default defineNuxtConfig({
     prerender: {
       crawlLinks: false,
       routes: ['/'],
-      ignore: ['/hi'],
+      autoSubfolderIndex: false,
     },
     devProxy: {
       '/api': {
