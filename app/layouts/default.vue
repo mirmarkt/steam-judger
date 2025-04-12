8163ed5457a930cd4febde6a0107a25feed8fa2e
Kutius <precious.wei@qq.com>
2025-04-13 06:26:24 +0800

    style: 调整默认布局和SteamIdInput组件的样式
    在默认布局中添加了`font-sans`类以统一字体样式，并简化了SteamIdInput组件中占位符的文本以提高可读性。

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-gray-900 bg-gray-50 flex flex-col h-screen dark:text-gray-100 dark:bg-gray-900">
+  <div class="text-gray-900 font-sans bg-gray-50 flex flex-col h-screen dark:text-gray-100 dark:bg-gray-900">
     <AppHeader />
 
     <main class="flex-1 w-full overflow-auto">
